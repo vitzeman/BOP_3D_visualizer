3b3783c5d36de0af9057076e3f272e8ec5e44349
Vit <vit.zeman.cz@gmail.com>
2025-03-20 20:21:20 +0100

    Refactor

---
+++ src/Prediction.py
@@ -0,0 +1,312 @@
+# Author: Vit Zeman
+# Czech Technical University in Prague, Czech Institute of Informatics, Robotics and Cybernetics, Testbed for Industry 4.0
+""" 
+Prediction class for handling the prediction from the BOP csv result file
+Additionaly controls the 2D visualization window for each prediction
+"""
+
+# Native imports
+import os
+import json
+import logging
+from typing import List, Tuple, Union
+from pathlib import Path
+
+import socket
+
+# Third party imports
+import cv2
+import numpy as np
+import open3d as o3d
+import open3d.visualization.gui as gui
+import pandas as pd
+
+
+from Models import Models
+
+os.makedirs("logs", exist_ok=True)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s | %(levelname)s | %(name)s | l: %(lineno)s | %(message)s",
+    handlers=[logging.FileHandler("logs/log.log", mode="w"), logging.StreamHandler()],
+)
+LOGGER = logging.getLogger(__name__)
+
+class Prediction:
+    """Handles the prediction files in csv format"""
+
+    def __init__(
+        self,
+        csv_path: Union[str, Path],
+        models: Models,
+        connection: socket.socket = None,
+        color: Tuple[float] = (0.5, 0.5, 0.5),
+    ) -> None:
+        """Initializes the prediction class, Loads the csv file and the default settings
+
+        Args:
+            csv_path (Union[str, Path]): Path to the csv file in BOP format, ie. methondName_datasetName-splitName.csv
+            models (Models): Models class with the models in the dataset
+            color (Tuple[float]): Color of the annotation objects in the scene (R,G,B) in range 0-1
+        """
+
+        # INFO: Connection used for the 2D visualization request
+        if connection is not None:
+            self.conn = connection
+
+        csv_path = Path(csv_path)
+        assert csv_path.exists(), f"Path {csv_path} does not exist"
+
+        df = pd.read_csv(csv_path)
+        self.dataframe: pd.DataFrame = df
+
+        file_name = csv_path.name
+        self.method_name: str = file_name.split("_")[0]
+        self.dataset_name: str = file_name.split("_")[1].split("-")[0]
+
+        self.color: Tuple[float] = color
+
+        self.models_set: Models = models
+
+        self._is_window2D_initialized: bool = False
+
+        self._material = o3d.visualization.rendering.MaterialRecord()
+        self._material.base_color = [1.0, 1.0, 1.0, 1.0]
+        self._material.shader = "defaultLit"
+        self.window2D = None
+
+        self.overlay = None
+        self.contour = None
+
+    def _init_window2D(self, image: np.ndarray) -> None:
+        """Initializes the 2D visualization window
+
+        Args:
+            image (np.ndarray): Image for the 2D visualization [HxWx3]
+        """
+        resolution = (image.shape[1], image.shape[0])
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        img_o3d = o3d.geometry.Image(rgb)
+
+        self.window2D = gui.Application.instance.create_window(
+            f"2D visualization {self.method_name}", *resolution
+        )
+        self.window2D_widget = gui.ImageWidget(img_o3d)
+        self.window2D.add_child(self.window2D_widget)
+        self.window2D.show(self._checkbox.checked)
+
+        self.window2D.set_on_close(self._igonore_close)
+
+        self._is_window2D_initialized = True
+
+    def _igonore_close(self) -> None:
+        """Ignores the close event of the 2D visualization window"""
+        pass
+
+    def _update_window2D(self, image: np.ndarray) -> None:
+        """Updates the image in the 2D visualization window
+
+        Args:
+            image (np.ndarray): Image for the 2D visualization [HxWx3]
+        """
+        # self.window2D.show(True)
+        img_o3d = o3d.geometry.Image(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        self.window2D_widget.update_image(img_o3d)
+        self.window2D.post_redraw()
+
+    def _hide_window2D(self) -> None:
+        """Hides the 2D visualization if 2D visualization is not required"""
+        self.window2D.show(False)
+
+    def _show_window2D(self) -> None:
+        """Shows the 2D visualization if 2D visualization is required"""
+        self.window2D.show(True)
+
+    def set_color(self, color: Tuple[float]) -> None:
+        """Sets the color of the annotation objects in the scene
+
+        Args:
+            color (Tuple[float]): Color of the annotation objects in the scene (R,G,B) in range 0-1
+        """
+        self.color = color
+
+    def _update_color(self, color: o3d.visualization.gui.Color) -> None:
+        """Updates the color of the annotation objects in the scene
+
+        Args:
+            color (o3d.visualization.gui.Color): Color of the annotation objects in the scene (R,G,B) in range 0-1
+        """
+        # print(f"Updating the color to {color.red, color.green, color.blue}")
+        self.color = (color.red, color.green, color.blue)
+
+    def _load_predictions(self, scene_id: int, image_id: int) -> pd.DataFrame:
+        """Loads the predictions for the given scene and image
+
+        Args:
+            scene_id (int): Index of the scene
+            image_id (int): Index of the image
+        """
+        predictions = self.dataframe.loc[
+            (self.dataframe["scene_id"] == scene_id)
+            & (self.dataframe["im_id"] == int(image_id))
+        ]
+
+        return predictions
+
+
+    def draw_curr_window2D(self):
+        """Draws the current overlay and contour images in the 2D window, also should be fasetr for the change of color"""
+        assert self.mask is not None, "Mask must be provided for the 2D visualization"
+        assert self.contours is not None, "Contours must be provided for the 2D visualization"
+        
+        img = cv2.imread(str(self.image_path))
+        color = [255 * x for x in self.color][::-1]
+        masked_img = np.zeros_like(img)
+
+        masked_img[self.mask] = color
+        overlay_img = img.copy()
+        overlay = cv2.addWeighted(overlay_img, 1, masked_img, 1, 0)
+        
+        contour_img = img.copy()
+        cv2.drawContours(contour_img, self.contours, -1, color, 2)
+        together = np.concatenate((overlay, contour_img), axis=0) 
+        if self._is_window2D_initialized:
+            self._update_window2D(together)
+        else:
+            self._init_window2D(together)
+
+
+    def _save_images(self, direcory_path: Union[str, Path] = "") -> None:
+        """Saves the contour and overlay images to the given directory
+
+        Args:
+            direcory_path (Union[str, Path]): Path to the directory where the images will be saved
+        """
+        if self.contour is None or self.overlay is None:
+            LOGGER.warning("No overlay and contour to save")
+            return
+
+        dir_path = Path(direcory_path)
+        dir_path.mkdir(parents=True, exist_ok=True)
+
+        contour_name = (
+            dir_path / f"{self.dataset_name}_{self.method_name}_s{str(self.scene_id).zfill(6)}_i{str(self.image_id).zfill(6)}_contour.png"
+        )
+        overlay_name = (
+            dir_path / f"{self.dataset_name}_{self.method_name}_s{str(self.scene_id).zfill(6)}_i{str(self.image_id).zfill(6)}_overlay.png"
+        )
+        if not contour_name.exists():
+            cv2.imwrite(str(contour_name), self.contour)
+        if not overlay_name.exists():    
+            cv2.imwrite(str(overlay_name), self.overlay)
+
+    def get_predictions(
+        self,
+        scene_id: int,
+        image_id: int,
+        plot2D: bool = False,
+        Kmx: np.ndarray = None,
+        image_path: Union[Path, str] = None,
+    ) -> Union[
+        List[o3d.geometry.TriangleMesh],
+        Tuple[List[o3d.geometry.TriangleMesh], np.ndarray, np.ndarray],
+    ]:
+        """Returns the predictions for the given scene and image
+
+        Args:
+            scene_id (int): Index of the scene
+            image_id (int): Index of the image
+            plot2D (bool): If True, the 2D visualization is returned with the overlay and contour
+            Kmx (np.ndarray): Camera matrix for the 2D visualization [3x3]
+            image (np.ndarray): Image for the 2D visualization [HxWx3]
+
+        Returns:
+            Union[List[o3d.geometry.TriangleMesh],Tuple[List[o3d.geometry.TriangleMesh], np.ndarray, np.ndarray]: List of the annotation objects in the scene or Tuple of the annotation objects, overlay and contour if plot2D is True
+        """
+        self.image_path = image_path
+        self.scene_id = scene_id
+        self.image_id = image_id
+        predictions = self._load_predictions(scene_id, image_id)
+
+        geom_list = [] # List with open3d geometry objects used for the visualization
+        objects_poses = []
+        for e, row in predictions.iterrows():
+            obj_id = str(row["obj_id"])
+            Rmx = (
+                np.array([x for x in row["R"].split(" ") if x != ""])
+                .astype(np.float32)
+                .reshape(3, 3)
+            )
+            tv = (
+                np.array([x for x in row["t"].split(" ") if x != ""])
+                .astype(np.float32)
+                .reshape(3, 1)
+            ) / 1000  # Convert to meters
+            pred_model = self.models_set.get_model(obj_id)
+
+            Tmx = np.eye(4)
+            Tmx[:3, :3] = Rmx
+            Tmx[:3, 3] = tv.flatten()
+
+            pred_model.scale(1 / 1000, center=(0, 0, 0))
+            pred_model.transform(Tmx)
+            pred_model.paint_uniform_color(self.color)
+
+            objects_poses.append({"obj_id": obj_id, "Tmx": Tmx.tolist()})
+
+            geom_list.append(pred_model)
+
+        if plot2D:
+            assert Kmx is not None, "K matrix must be provided for 2D visualization"
+            assert Kmx.shape == (
+                3,
+                3,
+            ), f"Intrinsic matrix K must be 3x3 not {Kmx.shape}"
+            assert image_path is not None, "Image must be provided for 2D visualization"
+            assert os.path.isfile(
+                image_path
+            ), f"Image path {image_path} is not a valid file"
+
+            if self.conn is not None:
+                d = {
+                    "image_path": str(image_path),
+                    "Kmx": Kmx.tolist(),
+                    "color": self.color,
+                    "objects_poses": objects_poses,
+                }
+                data = json.dumps(d).encode() + b"\n"
+
+                LOGGER.debug("Sending the data to the slave")
+                self.conn.sendall(data)
+                
+                response = ""
+                while True:
+                    b = self.conn.recv(1024).decode()
+                    response += b
+                    last = b[-1]
+                    if last == "\n":
+                        break
+
+                response = json.loads(response)
+
+                mask = response.get("mask", None)
+                contours = response.get("contours", None)
+                contours = [np.array(c) for c in contours]
+
+                self.mask = mask
+                self.contours = contours
+
+                if self.mask is not None and self.contours is not None:
+                    self.draw_curr_window2D()
+                else:
+                    LOGGER.error("No overlay and contour received")
+            else:
+                LOGGER.error("No connection to the 2D visualization server")
+                self.mask = None
+                self.contours = None
+        else:
+            if self._is_window2D_initialized:
+                self.window2D.show(False)
+
+        return geom_list
---
+++ src/predictions_2D_renderer.py
@@ -0,0 +1,233 @@
+# Author: Vit Zeman
+# Czech Technical University in Prague, Czech Institute of Informatics, Robotics and Cybernetics, Testbed for Industry 4.0
+
+"""
+Contains the prediction visualizer file for the 2D projections of the predictions.
+"""
+from typing import  Tuple, Union
+from pathlib import Path
+import socket
+import copy
+import os
+import logging
+import time
+import argparse
+
+# Third party imports
+import cv2
+import numpy as np
+import open3d as o3d
+import json
+
+# Custom imports
+from Models import Models
+
+
+os.makedirs("logs", exist_ok=True)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s | %(levelname)s | %(name)s | l: %(lineno)s | %(message)s",
+    handlers=[logging.FileHandler("logs/log.log", mode="w"), logging.StreamHandler()],
+)
+LOGGER = logging.getLogger(__name__)
+
+LOCAL_HOST_IP = "127.0.0.1"
+
+
+class PredictionVisualizerTwoD:
+
+    def __init__(
+        self,
+        path2models: Union[str, Path],
+        path2split: Union[str, Path],
+        s: socket.socket = None,
+    ):
+
+        self.mtl = o3d.visualization.rendering.MaterialRecord()
+        self.mtl.base_color = [1.0, 1.0, 1.0, 1.0]  # RGBA
+        self.mtl.shader = "defaultUnlit"
+
+        self.models = Models(path2models)
+
+        self.image_paths = Path(path2split)
+        self.s = s
+
+        self.last_img_path:str = ""
+
+    def render2D(
+        self,
+        img: np.ndarray,
+        Kmx: np.ndarray,
+        objects_poses: dict,
+        color: tuple = (0, 255, 0),
+    ) -> Tuple[np.ndarray]:
+        """Renders the images with 2D overlay and contour highlighting the object instances.
+
+        Args:
+            img (np.ndarray): Input image
+            Kmx (np.ndarray): Intrinsics matrix
+            objects_poses (dict): Dictionary with object ids as keys and poses as values.
+            color (tuple, optional): Color of the contour and overlay. Defaults to (0, 255, 0).
+        """
+
+        height, width = img.shape[:2]
+        pinhole = o3d.camera.PinholeCameraIntrinsic(
+            width, height, Kmx[0, 0], Kmx[1, 1], Kmx[0, 2], Kmx[1, 2]
+        )
+        renderer = o3d.visualization.rendering.OffscreenRenderer(width, height)
+        renderer.scene.set_background([0.0, 0.0, 0.0, 0.0])
+        renderer.scene.scene.set_sun_light([-1, -1, -1], [1.0, 1.0, 1.0], 100000)
+
+        contour_img = copy.deepcopy(img)
+        masked_img = np.zeros_like(img, dtype=np.uint8)
+
+        all_contours = ()
+        bin_mask = np.zeros_like(img[:, :, 0], dtype=np.uint8)
+        for e, object_pose in enumerate(objects_poses):
+            # print(object_pose)
+            obj_id = object_pose["obj_id"]
+            Tmx = object_pose["Tmx"]
+
+            model = self.models.get_model(obj_id)
+            model.scale(1 / 1000, center=(0, 0, 0))
+            model.transform(Tmx)
+
+            name = f"model_{obj_id}_{e}"
+            model.paint_uniform_color((1, 1, 1))  # white color
+            renderer.scene.add_geometry(name, model, self.mtl)
+            renderer.setup_camera(pinhole, np.eye(4))
+
+            img_o3d = renderer.render_to_image()
+            bgr = np.array(img_o3d)[:, :, ::-1]  # to BGR
+            img_gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
+            _, thresh = cv2.threshold(img_gray, 50, 255, 0)
+            contours, _ = cv2.findContours(
+                thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+            )
+            all_contours += contours
+            cv2.drawContours(contour_img, contours, -1, color, 2)
+            renderer.scene.remove_geometry(name)
+            mask = (bgr > 50)[:, :, 0]
+            bin_mask = np.logical_or(bin_mask, mask)
+            # Paint the colored mask into the masked image
+            masked_img[mask] = color
+
+    
+        return bin_mask, all_contours
+
+    def communication_loop(self, port: int):
+        """Starts the communication loop for the prediction visualizer.
+
+        Args:
+            port (int): Port number for the communication.
+        """
+        while True:
+            bytes = ""
+
+            while True:
+                b = self.s.recv(1024).decode()
+                bytes += b
+                last = b[-1]
+                if last == "\n":
+                    break
+
+            data = bytes
+            data = json.loads(data)
+            if "exit" in data.keys():
+                LOGGER.info("Exiting the 2D visualizer. ")
+                break
+
+            img_path = data["image_path"]
+            Kmx = np.array(data["Kmx"])
+            objects_poses = data["objects_poses"]
+            color = data["color"]
+            color = [255 * x for x in color][::-1]
+            if self.last_img_path != img_path:
+                img = cv2.imread(img_path) 
+                self.overlay, contours = self.render2D(img, Kmx, objects_poses, color)
+                self.contours = tuple(c.tolist() for c in contours)
+
+                    
+            else:
+                LOGGER.info("Image path is the same as the last one. Skipping the rendering.")
+
+            self.last_img_path = img_path
+            overlay = self.overlay.tolist()
+            contour_img = self.contours
+
+            images = {"mask": overlay, "contours": contour_img}
+            back = json.dumps(images).encode() + b"\n"
+            LOGGER.debug("Sending back the overlay and contour images.")
+            self.s.sendall(back)
+
+
+def demo_from_computer():
+    split_scene_path = "/home/vit/CIIRC/bop_toolkit/clearpose_downsample_100_bop/test"
+    models_path = "/home/vit/CIIRC/bop_toolkit/clearpose_downsample_100_bop/models"
+
+    pv = PredictionVisualizerTwoD(models_path, split_scene_path)
+    print("Visualizer initialized.")
+    data_path = "/home/vit/CIIRC/bop_toolkit/data.json"
+    data = json.load(open(data_path, "r"))
+
+    img_path = data["img_path"]
+    Kmx = np.array(data["Kmx"])
+    objects_poses = data["object_poses"]
+    color = data["color"]
+    color = [255 * x for x in color]
+
+    img = cv2.imread(img_path)
+    cv2.imshow("Input", img)
+
+    overlay, contour_img = pv.render2D(img, Kmx, objects_poses, color)
+
+    cv2.imshow("Overlay", overlay)
+    cv2.imshow("Contour", contour_img)
+
+    cv2.waitKey(0)
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Run the prediction visualizer scripts.")
+    parser.add_argument(
+        "--config",
+        "-c",
+        type=str,
+        required=True,
+        help="Path to the configuration file.",
+    )
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    config_path = Path(args.config)
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    host = LOCAL_HOST_IP
+    port = 65432 
+
+    host = config.get("host", host)
+    port = config.get("port", port)
+    
+    split_scene_path = config.get("split_scenes_path", None)
+    models_path = config.get("models_path", None)
+
+    assert split_scene_path is not None, "Split scene path is not provided."
+    assert models_path is not None, "Models path is not provided."
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    LOGGER.info(f"Trying to connect on {host}:{port}")
+    while True:
+        try:
+            s.connect((host, port))
+            break
+        except Exception as e:
+            LOGGER.warning(f"Connection on {host}:{port} failed. Retrying...")
+            time.sleep(1)
+    
+
+    LOGGER.info(f"Connection established on {host}:{port}")
+
+    pv = PredictionVisualizerTwoD(models_path, split_scene_path, s=s)
+    pv.communication_loop(port)
